42132e39954170b874951743a987c8f99c3662a2
xryuseix <ryusei143.shootingstar@gmail.com>
2020-02-16 15:51:41 +0900

    fetch picture url

--- source/fetch.py
+++ source/fetch.py
@@ -15,17 +15,17 @@ def download_file(url, dst_path):
     except urllib.error.URLError as e:
         print(e)
 
-url = 'https://pbs.twimg.com/profile_images/1160071194390831106/1JLRe3JE_200x200.jpg'
-dst_path = 'py-logo.png'
-download_file(url, dst_path)
+# url = 'https://pbs.twimg.com/profile_images/1160071194390831106/1JLRe3JE_200x200.jpg'
+# dst_path = 'py-logo.png'
+# download_file(url, dst_path)
 
 
-# url = 'https://twitter.com/ryusei_ishika'
-# path = './storage/downloaded_images/'
-# xpath_string = '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div/div/div/div[1]/div/div[1]/a/div[1]/div[2]/div/img'
-# css_string = 'css-901oao css-16my406 r-gwet1z r-ad9z0x r-bcqeeo r-qvutc0'
+url = 'https://twitter.com/ryusei_ishika'
+path = './storage/downloaded_images/'
+xpath_string = '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div/div/div/div[1]/div/div[1]/a/div[1]/div[2]/div/img'
+css_string = 'css-1dbjc4n r-sdzlij r-1p0dtai r-1mlwlqe r-1d2f490 r-1udh08x r-u8s1d r-zchlnj r-ipm5af r-417010'
 
-# driver = webdriver.Chrome()
+driver = webdriver.Chrome()
 
 # # ヘッドレスモード
 # # options = webdriver.ChromeOptions()
@@ -33,13 +33,9 @@ download_file(url, dst_path)
 # # driver = webdriver.Chrome(options=options)
 
 
-# driver.get('https://pbs.twimg.com/profile_images/1160071194390831106/1JLRe3JE_200x200.jpg')
-# time.sleep(1)
+driver.get(url)
+time.sleep(1)
 
-# src = driver.find_elements_by_xpath(xpath_string).get_attribute('src')
-# for a in driver.find_elements_by_xpath(xpath_string):
-#     src = a.get_attribute('src')
-#     urllib.request.urlretrieve(src, 'logo.png')
-#     print(type(a))
+print(driver.find_element_by_xpath(xpath_string).get_attribute("src"))
 
-
+driver.close()
