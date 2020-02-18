18b5b72e63aa5122c9be3d38869bf7417b4c6a65
xryuseix <ryusei143.shootingstar@gmail.com>
2020-02-18 16:52:15 +0900

    日本語対応した

--- source/generator.py
+++ source/generator.py
@@ -1,11 +1,14 @@
 import cv2
 import matplotlib.pyplot as plt
+from PIL import ImageFont, ImageDraw, Image
 import numpy as np
 import fetch
 import read_csv
 import re
 from tqdm import tqdm
 
+def is_japanese(str):
+    return True if re.search('[ぁ-んァ-ン]', str) else False 
 
 def make_image(username, twitter_id = 'rippro_logo_small'):
     path = "./storage/default_images/template.png"
@@ -21,49 +24,71 @@ def make_image(username, twitter_id = 'rippro_logo_small'):
     img2 = cv2.imread(icon)
     fig = plt.imshow(img1)
     plt.imshow(img2)
-    
+
     # アイコン画像のサイズ変更
-    img2 = cv2.resize(img2,(190,190))
+    img2 = cv2.resize(img2,(379,379))
     
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
-
     large_img = img1
     small_img = img2
-    plt.axis('off')
+    # plt.axis('off')
     fig.axes.get_xaxis().set_visible(False)
     fig.axes.get_yaxis().set_visible(False)
 
     # アイコン画像の左上の座標を決めて，テンプレート画像に重ねる
-    x_offset=35
-    y_offset=204
+    x_offset=69
+    y_offset=408
     large_img[y_offset:y_offset+small_img.shape[0], x_offset:x_offset+small_img.shape[1]] = small_img
 
     max_char = 11 # 枠に収まる最大の文字数
-    name_left = 240 # 名前の枠の左端
-    name_right = 670 # 名前の枠の右端
+    name_left = 450 # 名前の枠の左端
+    name_right = 1340 # 名前の枠の右端
+    name_height = 580 # 名前の高さ
     username_size = len(username) # 名前文字数
-    font_size = 2.0 # 名前のフォントの大きさ
-    char_size = 31 # 一文字単位の文字サイズ
+    font_size = 3.8 # 名前のフォントの大きさ
+    font = ImageFont.truetype('/Library/Fonts/Arial Unicode.ttf', 138) # フォントの指定/サイズ
+    char_size = 62 # 一文字単位の文字サイズ
+    
     if username_size > max_char:
-        font_size = 1.5
-        char_size = 23
+        font_size = 2.7
+        char_size = 44
+    if is_japanese(username):
+        char_size *= 1.5
 
+    # テキスト表示位置
     name_start_pos = ((name_right - name_left) - (username_size * char_size))//2 + name_left
-    cv2.putText(large_img, username, (name_start_pos, 340), cv2.FONT_HERSHEY_SIMPLEX, font_size, (0, 0, 0), thickness=3, lineType=cv2.LINE_AA)
+    position = (name_start_pos, name_height)
+
+    img_pil = Image.fromarray(large_img) # 配列の各値を8bit(1byte)整数型(0～255)をPIL Imageに変換。
+    draw = ImageDraw.Draw(img_pil) # drawインスタンスを生成
+
+    
+    draw.text(position, username, font = font , fill = (0, 0, 0, 0) ) # drawにテキストを記載 fill:色 BGRA (RGB)
+    large_img = np.array(img_pil)
+
     if twitter_id != 'rippro_logo_small':
-        cv2.putText(large_img, '@' + twitter_id, (30, 415), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), thickness=1, lineType=cv2.LINE_AA)
+        cv2.putText(large_img, '@' + twitter_id, (60, 830), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 0), thickness=2, lineType=cv2.LINE_AA)
 
     plt.imshow(large_img)
-    # plt.show()
-    plt.savefig('./storage/generated_images/' + username + '.png', bbox_inches='tight', pad_inches = 0)
+    plt.show()
+    plt.savefig('./storage/generated_images/' + username + '.png', bbox_inches='tight', pad_inches = 0.1)
+    return large_img
 
-csv = read_csv.read_csv()
+def images():
+    image_download = False
+    images = []
+    csv = read_csv.read_csv()
 
-for row in tqdm(csv):
-    s = row['Twitter ID（Twitterアイコンを名札の画像に使用します。記入がない場合、画像なしの名札になります）']
-    s = re.sub('^@', '', s)
-    row['Twitter ID（Twitterアイコンを名札の画像に使用します。記入がない場合、画像なしの名札になります）'] = s
-    # print(s)
-    fetch.download_images(row['Twitter ID（Twitterアイコンを名札の画像に使用します。記入がない場合、画像なしの名札になります）'])
-    make_image(row['名前またはハンドルネーム（名札で使用します）'], row['Twitter ID（Twitterアイコンを名札の画像に使用します。記入がない場合、画像なしの名札になります）'])
+    for row in tqdm(csv):
+        s = row['Twitter ID（Twitterアイコンを名札の画像に使用します。記入がない場合、画像なしの名札になります）']
+        s = re.sub('^@', '', s)
+        if row['名前またはハンドルネーム（名札で使用します）']=='handlename2':
+            row['名前またはハンドルネーム（名札で使用します）']='流流流流'
+        row['Twitter ID（Twitterアイコンを名札の画像に使用します。記入がない場合、画像なしの名札になります）'] = s
+        if image_download:
+            fetch.download_images(row['Twitter ID（Twitterアイコンを名札の画像に使用します。記入がない場合、画像なしの名札になります）'])
+        image = make_image(row['名前またはハンドルネーム（名札で使用します）'], row['Twitter ID（Twitterアイコンを名札の画像に使用します。記入がない場合、画像なしの名札になります）'])
+        images.append(image)
+    
+    return images
--- source/image_to_pdf.py
+++ source/image_to_pdf.py
@@ -4,14 +4,20 @@ import sys
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4, landscape, portrait
 from PIL import Image
+import generator
 
 path = "./storage/generated_images/handlename1.png"
-image =Image.open(path)
+generator.images()
+image = Image.open(path)
 pdf = canvas.Canvas("sample.pdf")
+# image = images[1]
+# import matplotlib.pyplot as plt
+# plt.imshow(image)
+# plt.show()
+# size_rate = 2.5
+# image = image.resize((int(image.width / size_rate), int(image.height / size_rate)))
 
-size_rate = 2.5
-image = image.resize((int(image.width / size_rate), int(image.height / size_rate)))
-pdf.drawInlineImage(image, 30, 200)
+pdf.drawInlineImage(image, 30, 200, width=259.2, height=172.1)
 
 
 
